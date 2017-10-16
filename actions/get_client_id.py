8cd52f60301dfb8181d781c15b82ecee7352b5c4
Martin Devlin <martin.devlin@pearson.com>
2017-10-16 19:04:25 +0100

    Fixes

--- actions/get_client_id.py
+++ actions/get_client_id.py
@@ -1,7 +1,7 @@
 from lib import action
 
 class KeycloakGetClientAction(action.KeycloakBaseAction):
-    def run(self, clientname):
+    def run(self, client_id_name):
 
-        client_id = self.keycloak_admin.get_client_id(clientname=clientname)
+        client_id = self.keycloak_admin.get_client_id(client_id_name=client_id_name)
         return client_id
