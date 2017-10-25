34ae026ea06f775a24ded763a1ba3b4ecfca18f7
Martin Devlin <martin.devlin@pearson.com>
2017-10-25 11:48:11 +0100

    Add password actions

--- actions/update_user.py
+++ actions/update_user.py
@@ -1,9 +1,9 @@
 from lib import action
 
 class KeycloakUpdateUserAction(action.KeycloakBaseAction):
-    def run(self, username, email='', firstName='', lastName='', enabled=True, emailVerified=False):
+    def run(self, username, email='', firstName='', lastName='', enabled=True, emailVerified=False, password=None, passwordTemp=True):
         user_id = self.keycloak_admin.get_user_id(username=username)
         if user_id != None:
-            self.keycloak_admin.update_user(user_id=user_id, username=username, email=email, firstName=firstName, lastName=lastName, emailVerified=emailVerified, enabled=enabled)
+            self.keycloak_admin.update_user(user_id=user_id, username=username, email=email, firstName=firstName, lastName=lastName, emailVerified=emailVerified, enabled=enabled, password=password, passwordTemp=passwordTemp)
         else:
             return "User " + username + " does not exist"
