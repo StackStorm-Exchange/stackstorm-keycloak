5bfef07f1c3c2064b7bc730bb74171e9762a735e
Martin Devlin <martin.devlin@pearson.com>
2017-10-27 11:29:29 +0100

    Add group actions

--- actions/get_group_id.py
+++ actions/get_group_id.py
@@ -1,7 +1,7 @@
 from lib import action
 
 class KeycloakGetGroupIdAction(action.KeycloakBaseAction):
-    def run(self, name):
+    def run(self, name=None, path=None):
 
-        group_id = self.keycloak_admin.get_group_id(name=name)
+        group_id = self.keycloak_admin.get_group_id(name=name, path=path)
         return group_id
