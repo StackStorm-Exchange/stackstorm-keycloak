6acb91252b8899cabd46421b370f7699a1954e9e
Martin Devlin <martin.devlin@pearson.com>
2017-10-31 23:17:21 +0000

    Add more group actions

--- actions/create_group.py
+++ actions/create_group.py
@@ -1,5 +1,5 @@
 from lib import action
 
 class KeycloakGroupCreateAction(action.KeycloakBaseAction):
-    def run(self, name, client_roles, realm_roles, sub_groups):
-        self.keycloak_admin.create_group(name=name, client_roles=client_roles, realm_roles=realm_roles, sub_groups=sub_groups)
+    def run(self, name, path, client_roles, realm_roles, sub_groups, parent):
+        self.keycloak_admin.create_group(name=name, path=path, client_roles=client_roles, realm_roles=realm_roles, sub_groups=sub_groups, parent=parent)
